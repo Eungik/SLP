8e2e40a4c6036a3642cc9d7c45898796a91bbee7
Eungik <dmddlrdl@snu.ac.kr>
2016-10-17 21:19:40 +0900

    dmddlrdl@snu.ac.kr

---
+++ Race/models.py
@@ -0,0 +1,56 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+
+import random
+
+author = 'Eungik Lee, Master Course Students in SNU'
+
+doc = """
+Racegame
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'Race'
+    players_per_group = None
+    num_rounds = 60
+    num_games = 6
+    ## Race game N in paper##
+    Winning_number=15
+    ## Race game k in paper ##
+    k=3
+    ##
+    ## winning reward##
+    Winning_reward=20
+    ##losing reward##
+    Losing_reward=10
+    timeout_sec = 10
+
+    winning_set = [p for p in range(1, Winning_number + 1) if (p % (4)) == 3]
+
+
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    Choice=models.PositiveIntegerField(min=1, max=Constants.Winning_number, initial=0)
+    computer_choice=models.PositiveIntegerField(initial=0)
+    previous_number=models.PositiveIntegerField(initial=0)
+    game_finished = models.BooleanField()
+
+    def ball_statuses(self):
+        return [x <= self.previous_number for x in range(1, Constants.Winning_number + 1)]
+
+
+
+
+
---
+++ Race/views.py
@@ -0,0 +1,101 @@
+from otree.api import Currency as c, currency_range
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+import random
+
+class Introduction(Page):
+
+
+    def is_displayed(self):
+        return self.subsession.round_number==1
+
+class BasePage(Page):
+    def is_displayed(self):
+        return self.participant.vars['games_finished'] < Constants.num_games and self.extra_is_displayed()
+
+    def extra_is_displayed(self):
+        return True
+
+
+class MyPage(BasePage):
+
+    form_model=models.Player
+    form_fields = ['Choice']
+
+    def vars_for_template(self):
+        if self.round_number > 1:
+            self.player.previous_number = self.player.in_round(self.round_number - 1).computer_choice
+        return {
+            'previous_number' : self.player.previous_number,
+            'round_number' : self.player.round_number,
+            'ball_statuses': self.player.ball_statuses(),
+            'ball_numbers': range(1, Constants.Winning_number + 1)
+            }
+
+    def Choice_min(self):
+        return self.player.previous_number + 1
+
+    def Choice_max(self):
+        return self.player.previous_number + 3
+
+
+
+    def before_next_page(self):
+
+        if self.player.Choice >= Constants.Winning_number - Constants.k:
+            self.player.game_finished = True
+            #
+        elif self.player.Choice in Constants.winning_set:
+            random_choice = random.randint(1, Constants.k)
+            self.player.computer_choice=self.player.Choice+ random_choice
+        else:
+            for n in Constants.winning_set:
+                if n >  self.player.Choice:
+                    self.player.computer_choice = n
+                    break
+
+
+    def is_displayed(self):
+        if self.round_number!=1:
+            return self.player.Choice < 15 and self.player.previous_number < 15 and self.player.computer_choice < 15
+        else:
+            return self.round_number==1
+
+
+class Computer(BasePage):
+
+    timeout_seconds = Constants.timeout_sec
+
+    def vars_for_template(self):
+            return{
+                'Choice': self.player.Choice,
+                'round_number': self.round_number
+             }
+
+    def is_displayed(self):
+        return not self.player.game_finished
+
+class Win(BasePage):
+    def is_displayed(self):
+        return self.player.Choice == 15
+
+    def before_next_page(self):
+        self.participant.vars['games_finished'] += 1
+
+
+class Lose(BasePage):
+    def is_displayed(self):
+        return 12 <= self.player.Choice < 15
+
+    def before_next_page(self):
+        self.participant.vars['games_finished'] += 1
+
+
+page_sequence = [
+    Introduction,
+    MyPage,
+    Computer,
+    Win,
+    Lose
+]
