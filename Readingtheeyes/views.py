dbb162b467c5afd7d52db959ee0440213d5c06fc
Eungik <dmddlrdl@snu.ac.kr>
2016-11-13 17:02:19 +0900

    dmddlrdl@snu.ac.kr

--- Readingtheeyes/views.py
+++ Readingtheeyes/views.py
@@ -7,7 +7,12 @@ class Introduction(Page):
     form_model = models.Player
     form_fields = ['eye_practice']
 
+class Introduction_Result(Page):
 
+    def vars_for_template(self):
+        return{
+        'eye_practice' : self.player.eye_practice
+        }
 
 class Question1(Page):
     form_model = models.Player
@@ -156,6 +161,7 @@ class Question28(Page):
 
 page_sequence = [
     Introduction,
+    Introduction_Result,
     Question1,
     Question2,
     Question3,
