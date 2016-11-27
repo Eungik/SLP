5be2bc3f5eddb3b4fa90be3ad38031357be552a1
Eungik <dmddlrdl@snu.ac.kr>
2016-11-27 22:48:04 +0900

    dmddlrdl@snu.ac.kr

--- Race_intro/views.py
+++ Race_intro/views.py
@@ -4,6 +4,8 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
+
+
 class Introduction(Page):
     def is_displayed(self):
         return self.round_number == 1
@@ -13,12 +15,21 @@ class Sample_intro(Page):
         return self.round_number == 1
 
 class Sample1(Page):
+
+    form_model=models.Player
+    form_fields = ['Practice']
+
     def is_displayed(self):
         return self.round_number == 1
 
 class Sample2(Page):
     def is_displayed(self):
         return self.round_number == 1
+
+    def vars_for_template(self):
+        return{
+            'practice': self.player.Practice
+        }
 
 class Sample3(Page):
     def is_displayed(self):
@@ -33,4 +44,5 @@ page_sequence = [
     Sample_intro,
     Sample1,
     Sample2,
+    Sample3,
 ]
